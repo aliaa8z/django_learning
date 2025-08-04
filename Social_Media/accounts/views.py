fcb13d6a730f943e33d11db5812daf4bbefb6bf3
aliaa8s <aliasaaa315@gmail.com>
2025-08-04 17:26:41 +0330

    vid 33

--- Social_Media/accounts/views.py
+++ Social_Media/accounts/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from home.models import Post
+from .models import Relation
 
 
 # Create your views here.
@@ -53,12 +54,18 @@ class UserLoginView(View):
             if user is not None:
                 login(request, user)
                 messages.success(request, 'you logged in successfully', 'success')
+                if self.next:
+                    return redirect(self.next)
                 return redirect('home:home')
             messages.error(request, 'username or password is wrong', 'warning')
         return render(request, self.template_name, {'form': form})
 
 
 class UserLogoutView(LoginRequiredMixin, View):
+    def setup(self, request, *args, **kwargs):
+        self.next = request.GET.get('next')
+        return super().setup(request, *args, **kwargs)
+
     def get(self, request):
         logout(request)
         messages.success(request, 'you logged out successfully', 'success')
@@ -67,7 +74,50 @@ class UserLogoutView(LoginRequiredMixin, View):
 
 class UserProfileView(LoginRequiredMixin, View):
     def get(self, request, user_id):
+        is_following = False
         user = User.objects.get(pk = user_id)
-        posts = Post.objects.filter(user = user)
-        return render(request, 'accounts/profile.html', {'user': user, 'posts': posts})
+        posts = user.posts.all()
+        relation = Relation.objects.filter(from_user= request.user, to_user= user)
+        if relation.exists():
+            is_following = True
+        return render(request, 'accounts/profile.html', {'user': user, 'posts': posts, 'is_following': is_following})
 
+
+class UserFollowView(LoginRequiredMixin, View):
+	def dispatch(self, request, *args, **kwargs):
+		user=User.objects.get(id=kwargs['user_id'])
+		if user.id != request.user.id:
+			return super().dispatch(request, *args, **kwargs)
+		else:
+			messages.error(request,'you cant follow/unfollow your account','danger')
+			return redirect('accounts:user_profile',user.id)
+
+	def get(self, request, user_id):
+		user = User.objects.get(id=user_id)
+		relation = Relation.objects.filter(from_user=request.user, to_user=user)
+		if relation.exists():
+			messages.error(request, 'you are already following this user', 'danger')
+		else:
+			Relation(from_user=request.user, to_user=user).save()
+			messages.success(request, 'you followed this user', 'success')
+		return redirect('accounts:user_profile', user.id)
+
+
+class UserUnFollowView(LoginRequiredMixin, View):
+	def dispatch(self, request, *args, **kwargs):
+		user=User.objects.get(id=kwargs['user_id'])
+		if user.id != request.user.id:
+			return super().dispatch(request, *args, **kwargs)
+		else:
+			messages.error(request,'you cant follow/unfollow your account','danger')
+			return redirect('accounts:user_profile',user.id)
+	
+	def get(self, request, user_id):
+		user = User.objects.get(id=user_id)
+		relation = Relation.objects.filter(from_user=request.user, to_user=user)
+		if relation.exists():
+			relation.delete()
+			messages.success(request, 'you unfollowed this user', 'success')
+		else:
+			messages.error(request, 'you are not following this user', 'danger')
+		return redirect('accounts:user_profile', user.id)
--- Social_Media/home/views.py
+++ Social_Media/home/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from .forms import PostUpdateForm
+from .forms import PostCreateUpdateForm
 from django.utils.text import slugify
 
 # Create your views here.
@@ -32,30 +32,48 @@ class PostDeleteView(LoginRequiredMixin, View):
 
 
 class PostUpdateView(LoginRequiredMixin, View):
-    form_class = PostUpdateForm
+	form_class = PostCreateUpdateForm
 
-    def setup(self, request, *args, **kwargs):
-        self.post_instance = Post.objects.get(pk= kwargs['post_id'])
-        return super().setup(request, *args, **kwargs)
+	def setup(self, request, *args, **kwargs):
+		self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'])
+		return super().setup(request, *args, **kwargs)
 
-    def dispatch(self, request, *args, **kwargs):
-        post = self.post_instance()
-        if not post.user.id == request.user.id:
-            messages.error(request, 'you cant update this post', 'danger')
-            return redirect('home:home')
-        return super().dispatch(request, *args, **kwargs)
+	def dispatch(self, request, *args, **kwargs):
+		post = self.post_instance
+		if not post.user.id == request.user.id:
+			messages.error(request, 'you cant update this post', 'danger')
+			return redirect('home:home')
+		return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request, *args, **kwargs): # we don't use post_id so we put args instead of it. 
-        post = self.post_instance
-        form = self.form_class(instance= post)
-        return render(request, 'home/update.html', {'form': form})
+	def get(self, request, *args, **kwargs):
+		post = self.post_instance
+		form = self.form_class(instance=post)
+		return render(request, 'home/update.html', {'form':form})
 
-    def post(self, request, post_id):
-        post = self.post_instance
-        form = self.form_class(request.POST, instance= post)
+	def post(self, request, *args, **kwargs):
+		post = self.post_instance
+		form = self.form_class(request.POST, instance=post)
+		if form.is_valid():
+			new_post = form.save(commit=False)
+			new_post.slug = slugify(form.cleaned_data['body'][:30])
+			new_post.save()
+			messages.success(request, 'you updated this post', 'success')
+			return redirect('home:post_detail', post.id, post.slug)
+
+
+class PostCreateView(LoginRequiredMixin, View):
+    form_class = PostCreateUpdateForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, 'home/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
         if form.is_valid():
             new_post = form.save(commit = False)
             new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.user = request.user
             new_post.save()
-            messages.success(request, 'post updated successfully', 'success')
-            return redirect('home:post_detail', post.id, post.slug)
+            messages.success(request, 'post created successfully', 'success')
+            return redirect('home:post_detail', new_post.id, new_post.slug)
